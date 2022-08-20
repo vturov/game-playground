ce231b14d736f41dfb4b04e18367755b6d3e156e
Vladimir Turov <vladimir.turov@stenn.com>
2022-08-20 02:06:21 +0200

    + Added base silk app setup

---
+++ Game/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var app = new Application();
+            app.Run(args);
+        }
+    }
+}

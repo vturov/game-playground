2248006811ce0e83fa2e22c85cd526473fc47a1f
Vladimir Turov <vladimir.turov@stenn.com>
2022-08-20 02:07:22 +0200

    * Adding explicit access modifier

--- Game/Program.cs
+++ Game/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using var app = new Application();
             app.Run(args);
